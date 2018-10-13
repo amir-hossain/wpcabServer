ec75e6c9ebd918bdc4e3e51f51fa5732ebe7ad18
amir <bladyamir@gmail.com>
2018-10-14 00:58:40 +0600

    insert mongo done

--- Wpcab server/Model/User.cs
+++ Wpcab server/Model/User.cs
@@ -14,5 +14,41 @@ namespace WpcabServer.Model
         public string FatherName { get; set; }
 
         public string MotherName { get; set; }
+
+        public string DOB { get; set; }
+        
+        public string MaritalStatus { get; set; }
+
+        public string SpouseName { get; set; }
+
+        public string Inviter { get; set; }
+
+        public string BloodGroup { get; set; }
+
+        public string Occupation { get; set; }
+
+        public string ParmanentAddress { get; set; }
+
+        public string Zone { get; set; }
+
+        public string SubDistrict { get; set; }
+
+        public string District { get; set; }
+
+        public string Country { get; set; }
+
+        public string Nationality { get; set; }
+
+        public string NID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
     }
 }
